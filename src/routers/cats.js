d2a7ceb10ed465684792aaf0e20dbbfb2dab0dee
Cooper Walsh <cooperwalsh76@gmail.com>
2021-05-12 01:22:26 +0000

    time to move enviroments

--- src/routers/cats.js
+++ src/routers/cats.js
@@ -22,62 +22,72 @@ const router = new express.Router()
 
 router.get('/cats', async (req, res)=> {
     const connection = hana.createConnection();
-    // await connection.connect(connOptions, (err)=>{
-    //     if(err){
-    //         res.status(400).send(err)
-    //     }
-    // });
-    // const sql = 'SELECT * FROM TEST_SCHEMA.CATS';
-    // try {
-    //         const rows = await connection.exec(sql);
-    //         res.status(200).send(rows)
-    //  } catch (e) {
-    //         res.status(400).send(e)
-    //  }
-        var tasks = [
-            myconn,
-            mysql, myexecute, myresults,
-            mydisco
-        ];
+    try{
+        await connection.connect(connOptions);
+    }catch(e){
+        res.status(400).send(e)
+    }
+    
+    const sql = 'SELECT * FROM TEST_SCHEMA.CATS';
+    try {
+            const rows = await connection.exec(sql);
+            res.status(200).send(rows)
+     } catch (e) {
+            res.status(400).send(e)
+     }
 
-        waterfall(tasks, done);
-        console.log("Async calls underway\n");
+     try{
+        await connection.disconnect();
+        console.log('db connection deleted');
+     }catch(e) {
+        res.status(400).send(e)
+     }
+
+     // OLD WATERFALL METHOD LISTED BELOW - NO LONGER NEEDED AS ASYNC AWAIT FRAMEWORK NOW IS FUNCTIONAL 
+        // var tasks = [
+        //     myconn,
+        //     mysql, myexecute, myresults,
+        //     mydisco
+        // ];
+
+        // waterfall(tasks, done);
+        // console.log("Async calls underway\n");
 
 
 
-        function myconn(cb) {
-            connection.connect(connOptions);
-            cb(null);
-        }
+        // function myconn(cb) {
+        //     connection.connect(connOptions);
+        //     cb(null);
+        // }
 
-        function mysql(cb) {
-            var sql = 'SELECT * FROM TEST_SCHEMA.CATS';
-            cb(null, sql);
-        }
+        // function mysql(cb) {
+        //     var sql = 'SELECT * FROM TEST_SCHEMA.CATS';
+        //     cb(null, sql);
+        // }
 
-        function myexecute(sql, cb) {
-            var rows = connection.exec(sql);
-            cb(null, rows);
-        }
+        // function myexecute(sql, cb) {
+        //     var rows = connection.exec(sql);
+        //     cb(null, rows);
+        // }
 
-        function myresults(rows, cb) {
-            console.log(util.inspect(rows, { colors: true }));
-            console.log("");
-            res.send(rows);
-            cb(null);
-        }
+        // function myresults(rows, cb) {
+        //     console.log(util.inspect(rows, { colors: true }));
+        //     console.log("");
+        //     res.send(rows);
+        //     cb(null);
+        // }
 
-        function mydisco(cb) {
+        // function mydisco(cb) {
             
-            connection.disconnect(cb);
-        }
+        //     connection.disconnect(cb);
+        // }
 
-        function done(err) {
-            console.log("Async done");
-            if (err) {
-                return console.error(err);
-            }
-        }
+        // function done(err) {
+        //     console.log("Async done");
+        //     if (err) {
+        //         return console.error(err);
+        //     }
+        // }
     
 
     
@@ -87,70 +97,36 @@ router.get('/newcat', (req, res)=>{
     res.render('newcat')
 })
 
-// router.post('/cats', async (req, res)=> {
-//     let name = req.body.cat 
-//     let age = req.body.cat
+router.post('/cats', async (req, res)=> {
+    let name = req.body.name;
+    let age = req.body.age;
     
-//     const connection = hana.createConnection();
-//     // await connection.connect(connOptions, (err)=>{
-//     //     if(err){
-//     //         res.status(400).send(err)
-//     //     }
-//     // });
-//     // const sql = 'SELECT * FROM TEST_SCHEMA.CATS';
-//     // try {
-//     //         const rows = await connection.exec(sql);
-//     //         res.status(200).send(rows)
-//     //  } catch (e) {
-//     //         res.status(400).send(e)
-//     //  }
-//         var tasks = [
-//             myconn,
-//             mysql, myexecute, myresults,
-//             mydisco
-//         ];
-
-//         waterfall(tasks, done);
-//         console.log("Async calls underway\n");
-
-
-
-//         function myconn(cb) {
-//             connection.connect(connOptions);
-//             cb(null);
-//         }
-
-//         function mysql(cb) {
-//             var sql = 'INSERT INTO TEST_SCHEMA.CATS';
-//             cb(null, sql);
-//         }
-
-//         function myexecute(sql, cb) {
-//             var rows = connection.exec(sql);
-//             cb(null, rows);
-//         }
-
-//         function myresults(rows, cb) {
-//             console.log(util.inspect(rows, { colors: true }));
-//             console.log("");
-//             res.send(rows);
-//             cb(null);
-//         }
-
-//         function mydisco(cb) {
-            
-//             connection.disconnect(cb);
-//         }
-
-//         function done(err) {
-//             console.log("Async done");
-//             if (err) {
-//                 return console.error(err);
-//             }
-//         }
+    const connection = hana.createConnection();
+    try{
+        await connection.connect(connOptions);
+    }catch(e){
+        res.status(400).send(e);
+    }
     
+    console.log("Name: ", name, " Age: ", age);
+    res.send("hello: ", name);
+
+    // const sql = "INSERT INTO TEST_SCHEMA.CATS(NAME, AGE) VALUES(?, ?)";
+    // try {
+    //         const rows = await connection.exec(sql, [name, age]);
+    //         res.status(201).send(rows);
+    //  } catch (e) {
+    //         res.status(400).send("The error happened during the post here is the error:",e);
+    //  }
+    
+    try{
+        await connection.disconnect();
+        console.log('db connection deleted');
+    }catch(e) {
+        res.status(400).send(e);
+    }
 
     
-// })
+})
 
 module.exports = router
